87d40aec02d996fb3ed7e6edd0dd06d9c4434512
vanessa Mayara <mayaravanessa620@gmail.com>
2021-06-25 17:34:44 -0300

    atualização3

--- src/app/directives/for.directive.ts
+++ src/app/directives/for.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[myFor]'
 })
 export class ForDirective implements OnInit{
 
-  @Input("myForEm") numbers:number[] | undefined
-  @Input("myForUsando") texto:string | undefined
-
-  constructor() {
+  @Input("myForEm") numbers:number[] | any
+ 
+  constructor(
+   private container:ViewContainerRef,
+    private template:TemplateRef<any>) {
    
    }
   ngOnInit(): void{
-    console.log(this.numbers)
-    console.log(this.texto)
-}
+    for(let number of  this.numbers) {
+     this.container.createEmbeddedView(
+     this.template,{$implicit:number});
+  } 
+ }
 }
