f3b9746d5b18c4ff2a03f290e2e1808006e0ff28
vanessa Mayara <mayaravanessa620@gmail.com>
2021-06-25 16:37:43 -0300

    atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import{ MatSidenavModule} from "@angular/material/sidenav";
 import{MatListModule} from "@angular/material/list";
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
